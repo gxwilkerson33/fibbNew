7ba9aca939b07e7b5b8ec547b9a2b8c79055acbf
garrettwilkerson <gwilkerson@firstorion.com>
2023-03-06 15:24:58 -0600

    first commit

---
+++ client/src/otherPage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import { Link } from 'react-router-dom'
+
+var test = () => {
+    return (
+        <div>
+            Im Some other Page
+            <Link to="/">Go Back Home</Link>
+        </div>
+    )
+
+}
+
+export default test
